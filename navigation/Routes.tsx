fe088a3fc1a2d60fa29d3b7f39581c2b8ba11360
josRosario <jrosario@intellisysdcorp.com>
2025-02-16 14:43:11 -0400

    feat/add-token

--- navigation/Routes.tsx
+++ navigation/Routes.tsx
@@ -1,10 +1,29 @@
 import AuthStack from "./AuthStack";
 import AppNavigator from "./AppNavigator";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 const Routes = () => {
   const [isLogued, setIslogued] = useState(false);
+
+  useEffect(() => {
+    const checkToken = async () => {
+      try {
+        const token = await AsyncStorage.getItem("token");
+        console.log("Token:", token);
+        if (token) {
+          setIslogued(true);
+        }
+      } catch (error) {
+        console.error("Error getting token:", error);
+      }
+    };
+    checkToken();
+  }, []);
+  
+
 
   return (
     <SafeAreaProvider isTVSelectable={true}>
